5ab35039c5c99230ae9895d0d41af184c46a8aff
Vladimir Vyalov <vyalov.v@gmail.com>
2022-08-30 21:15:54 +0300

    Add empty index handler

--- onx/server/app.py
+++ onx/server/app.py
@@ -3,8 +3,13 @@ from aiohttp import web
 from onx.server.handler import WebsocketHandler
 
 
+async def index_handler(_):
+    return web.json_response({})
+
+
 def get_application() -> web.Application:
     app = web.Application()
     app["websockets"] = []
+    app.router.add_route("GET", "/", index_handler)
     app.router.add_route("GET", "/ws", WebsocketHandler)
     return app
